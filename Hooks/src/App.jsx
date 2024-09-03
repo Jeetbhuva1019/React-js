44f1a02018e1d745db21aa4e21161aa42e9d251f
Jeetbhuva1019 <162433565+Jeetbhuva1019@users.noreply.github.com>
2024-09-03 11:06:53 +0530

    Day4

---
+++ Hooks/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Hooks from './Components/Hooks'
+
+function App() {
+  return (
+    <div>
+      <Hooks />
+    </div>
+  )
+}
+
+export default App
