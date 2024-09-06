7ac74f5a630191bd62975f80983de541a3281b52
Jeetbhuva1019 <162433565+Jeetbhuva1019@users.noreply.github.com>
2024-09-06 10:37:53 +0530

    Day7

---
+++ React Events and Conditional Rendering/src/App.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+import Condition from './Components/Condition'
+import E1 from './Components/E1'
+import E2 from './Components/E2'
+import E3 from './Components/E3'
+
+export default function app() {
+  return (
+    <div>
+      <E1 />
+      <E2 />
+      <E3 />
+      <Condition />
+    </div>
+  )
+}
+
