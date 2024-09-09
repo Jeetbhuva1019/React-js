6b91804fb2780c8b688eeecb84c6974d6ed99e1c
Jeetbhuva1019 <162433565+Jeetbhuva1019@users.noreply.github.com>
2024-09-09 16:18:29 +0530

    Day8

---
+++ List_Key_Ref_Fragment/src/App.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import Listkey from './Component/Listkey'
+import Ref from './Component/Ref'
+
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        <Listkey/>
+        <Ref/>
+      </div>
+    )
+  }
+}
