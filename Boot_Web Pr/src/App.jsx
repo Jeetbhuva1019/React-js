a55a127ae2e3ce6ec61b36597ccf1ed2bf6be1e8
Jeetbhuva1019 <162433565+Jeetbhuva1019@users.noreply.github.com>
2024-09-20 16:01:29 +0530

    Day 14

---
+++ Boot_Web Pr/src/App.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import Index from './Component/Index'
+// import About from './Component/About'
+
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        <Index />
+        {/* <About /> */}
+      </div>
+    )
+  }
+}
