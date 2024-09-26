4bf40eae710e177f4547135514c625ca09c0e143
Jeetbhuva1019 <162433565+Jeetbhuva1019@users.noreply.github.com>
2024-09-26 15:18:24 +0530

    Day17

---
+++ Full Curd/src/App.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import Full_Curd from './component/Full_Curd'
+
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        <Full_Curd />
+      </div>
+    )
+  }
+}
